5fc515978d1302e52b9df7b2d8581d0931f96947
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-07-18 15:26:42 +0930

    history search script
    assets/images/Screen1.png

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Weather_Forecast_App
+### Weather_Forecast_App
 Week 6 Challenge
+
+* ['Weather-Dashboard - Github']()
+* ['Weather-Dashboard - Github IO']()
+
+![](assets/images/Screen1.png)
+![](assets/images/Screen2.png)
+
+### Summary
+* HTML and CSS and Javascript documents create a weather application 
+* This project emphasizes the use of using an API call and JQuery to make dynamic changes to an HMTL document
+* This project utilizes the use of appending HTML pages 
+
+### This project has the following features: 
+* A Search bar for the city location
+* A card that provides the current weather
+    ** Location, Temp, Humidity, Wind Speed, UV Index 
+* A Card that provides a five day forecast  
+
+### Psuedo code:  
+* Create a search input for customer input 
+* 3 API calls for Current Weather, 5-day Forecast and UV Index
+* 3 Separate cards with search history, current weather and 5-day weather
+* Local storage will save user history 
+* .empty() will clear old info before new info is populated 
+
+### This project has script features of:
+* 3 API calls
+* Variable declaration area 
+* An event listener (onclick) that recieves user input
+* A ForEach to loop through the 5-day forecast 
+
+### To Execute File:
+> Open in browser
+
+### Features: 
+* One HTML Pages
+    * Index.html 
+        * Contains Search bar and placeholder divs for content
+* One CSS Page
+    * Styles.css
+        * Adds formatting to 5-day forecast and cards 
+* One Javascript Page
+        * Contains: 
+        * Variables, including array
+        * Event listeners
+        * For each loop
+        * Local Storage set and get 
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,79 +3,73 @@ var searchButton = $(".searchButton");
 
 var apiKey = "e70812a8d4a954f92633101b0d6a9635";
 
+var citiesFound = 0;
+
 // Forloop for persisting the data onto HMTL page
 for (var i = 0; i < localStorage.length; i++) {
-
     var city = localStorage.getItem(i);
-    // console.log(localStorage.getItem("City"));
-    var cityName = $(".list-group").addClass("list-group-item");
-
-    cityName.append("<li>" + city + "</li>");
 }
 // Key count for local storage 
 var keyCount = 0;
-// Search button click event
-searchButton.click(function () {
 
-    var searchInput = $(".searchInput").val();
+function showForecast(city) {
 
     // Variable for current weather working 
-    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=metric";
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&Appid=" + apiKey + "&units=metric";
     // Variable for 5 day forecast working
-    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=metric";
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&Appid=" + apiKey + "&units=metric";
 
-
-    if (searchInput == "") {
-        console.log(searchInput);
+    if (city == "") {
+        console.log(city);
+        
     } else {
         $.ajax({
             url: urlCurrent,
             method: "GET"
         }).then(function (response) {
             // list-group append an li to it with just set text
-            // console.log(response.name);
             var cityName = $(".list-group").addClass("list-group-item");
-            cityName.append("<li>" + response.name + "</li>");
+            
+            // increase found cities counter
             // Local storage
             var local = localStorage.setItem(keyCount, response.name);
             keyCount = keyCount + 1;
 
+            cityName.append("<li>" + "<button id=\"cityButton" + keyCount + "\">" + response.name + "</button>" + "</li>");           
+            $("#cityButton" + keyCount).addClass("cityButton");
+            $("#cityButton" + keyCount).click(() => {
+                showForecast(city);
+            });
+            
             // Start Current Weather append 
             var currentCard = $(".currentCard").append("<div>").addClass("card-body");
             currentCard.empty();
             var currentName = currentCard.append("<p>");
-            // .addClass("card-text");
             currentCard.append(currentName);
 
             // Adjust Date 
             var timeUTC = new Date(response.dt * 1000);
             currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
+            // Weather Icon
             currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
             // Add Temp 
             var currentTemp = currentName.append("<p>");
-            // .addClass("card-text");
             currentName.append(currentTemp);
             currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
             // Add Humidity
             currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
-            // // Add Wind Speed: 
+            // Add Wind Speed: 
             currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
-
             // UV Index URL
             var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
-
             // UV Index
             $.ajax({
                 url: urlUV,
                 method: "GET"
             }).then(function (response) {
-
                 var currentUV = (response.value);
-                var UV = String(currentUV);
-                currentTemp.append("<p>" + "UV Index: " + "<span>" + UV + "</span>" + "</p>").addClass("card-text");
-                
-                //console.log(UV);
-            // UV index coloring
+                currentTemp.append("<p>" + "UV Index: " + "<span>" + currentUV + "</span>" + "</p>").addClass("card-text fiveDayColor");                
+            // UV index color
                 if (currentUV <= 4) {
                    $('span').addClass("low");
                 } else if (response.value <= 7) {
@@ -83,11 +77,9 @@ searchButton.click(function () {
                 } else if (response.value <= 10) {
                     $('span').addClass("high");
                 }
-                }
-                
+                }            
             );
-
-        });
+        })};
 
         // Start call for 5-day forecast 
         $.ajax({
@@ -105,10 +97,14 @@ searchButton.click(function () {
                 FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
 
                 fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
-
-
             })
-
         });
-    }
+};
+
+// Search button click event
+searchButton.click(() => {
+    var searchInput = $(".searchInput").val();
+
+    showForecast(searchInput);
+
 });
